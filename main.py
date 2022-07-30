a7cfd76036417c1281cd8fcddb94092b65d751bb
Alex <i@zaharov-1.ru>
2022-07-30 22:25:49 +0500

    add faile

---
+++ main.py
@@ -0,0 +1,15 @@
+# передадим в программу данные
+
+print('Напишите про ветер:')
+beaufort = int(input())
+print('Напишите про дождь:')
+is_raining = str(input())
+print('Напишите про температуру:')
+temperature = int(input())
+
+if (is_raining == "нет" or is_raining == "Нет" and beaufort <= 4) and temperature > 16:
+    print('Идём гулять, на улице хорошо')
+else:
+    print('Сидим дома, читаем книги')
+
+input()
